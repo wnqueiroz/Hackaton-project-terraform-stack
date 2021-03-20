035176db2e9dfe50993eadeb80956d9b3f9abc7d
William Queiroz <wnqueiroz@outlook.com>
2021-03-20 18:34:18 +0000

    ci: change AWS S3 bucket

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackathon-fiap-76aoj-337626"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
